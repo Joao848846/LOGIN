e2e71cf06a7ec2cfc78fc3896deb40f66d128ab3
JoaoDasilva <joaovictornunessilva12345@gmail.com>
2023-01-14 21:51:02 -0300

    .

---
+++ login.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Formulario</title>
+</head>
+<body>
+ 
+    <div class="container" >
+        <a class="links" id="paracadastro"></a>
+        <a class="links" id="paralogin"></a>
+        
+        <div class="content">      
+          <!--FORMULÁRIO DE LOGIN-->
+          <div id="login">
+            <form method="post" action=""> 
+              <h1>PLATAFORMA</h1>
+              <h1>MKOM</h1>
+              <p> 
+                <form class="login.php" method="post"></form>
+                <label for="email_login">E-mail</label>
+                <input id="email_login" name="email_login" required="required" type="text" placeholder="Digite seu e-mail"/>
+              </p>
+              
+              <p> 
+                    <LAbel for="senha"> Senha</LAbel>
+                <input id="senha_login" name="senha_login" required="required" type="password" placeholder="Sua senha" /> 
+              </p>
+              
+              <p> 
+                <input type="checkbox" name="manterlogado" id="manterlogado" value="" /> 
+                <label for="manterlogado">Manter-me logado</label>
+              </p>
+              
+              <p> 
+                <input type="submit" value="Entrar" /> 
+              </p>
+              
+              <p class="link">
+                
+                <a href="#paracadastro">Cadastre-se</a>
+              </p>
+            </form>
+          </div>
+    
+          <!--FORMULÁRIO DE CADASTRO-->
+          <div id="cadastro">
+            <form method="post" action=""> 
+              <H1>PLATAFORMA</H1>
+              <h1>MKOM</h1>
+              
+              <p> 
+                <label for="nome_cad">Usuário</label>
+                <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="Seu nome" pattern="[A-Za-z]{4}" />
+              </p>
+              
+              <p> 
+                <label for="email_cad">E-mail</label>
+                <input id="email_cad" name="email_cad" required="required" type="email" placeholder="Digite seu e-mail" pattern="[A-Zz-a]{4}"/> 
+              </p>
+              
+              <p> 
+                <label for="senha_cad">Senha</label>
+                <input id="senha_cad" name="senha_cad" required="required" aria-required="true" type="password" placeholder="Ex:1234" min="10" max="15" step="2" value="10"/>
+              </p>
+              
+              <p> 
+                <input type="submit" value="Criar conta"/> 
+              </p>
+              
+              <p class="link">  
+                
+                <a href="#paralogin"> Ir para Login </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div> 
+</body>
+
